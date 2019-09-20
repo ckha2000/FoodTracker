ef4107e91fb5b4cd9e5b7eccc13201c6ff65e762
ck <christopher.kha2000@gmail.com>
2019-09-19 17:37:55 -0700

    Initial Commit

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by christopher kha on 9/1/19.
+//  Copyright © 2019 christopher kha. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodTrackerUITests/FoodTrackerUITests.swift
@@ -0,0 +1,13 @@
+//
+//  FoodTrackerUITests.swift
+//  FoodTrackerUITests
+//
+//  Created by christopher kha on 9/1/19.
+//  Copyright © 2019 christopher kha. All rights reserved.
+//
+
+import XCTest
+
+class FoodTrackerUITests: XCTestCase {
+
+}
